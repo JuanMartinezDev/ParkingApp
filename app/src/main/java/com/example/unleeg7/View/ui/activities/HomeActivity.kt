2d49e2a3eed241bdf271c8cd86980883c0ed2ddb
Juan Manuel Martinez Martinez <juanmartinez@sqdm.com>
2022-11-03 18:17:59 -0500

    primer commit

---
+++ app/src/main/java/com/example/unleeg7/View/ui/activities/HomeActivity.kt
@@ -0,0 +1,18 @@
+package com.example.unleeg7.View.ui.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.unleeg7.R
+
+class HomeActivity:AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+
+    /*override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.nav_book,menu)
+        return true
+    }*/
+}
